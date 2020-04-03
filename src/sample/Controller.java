1b4c100eba97d0664365f1aadbb4182029445ea5
Chaldun <chaldunalder@gmail.com>
2020-04-02 19:32:36 -0500

    test commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,5 +8,6 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //testing
     }
 }
