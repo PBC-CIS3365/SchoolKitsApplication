62bc9b9ed4250ca3a53524409be2de56d8049b15
Chaldun <chaldunalder@gmail.com>
2020-04-11 02:13:20 -0500

    Create scene to replicate framework provided. Purely Cosmetic .. more to come
    https://xd.adobe.com/view/7793d231-4e7c-45d4-446a-f3f877c8d708-01a2/screen/521e550f-7ba7-4eaf-86f3-fa64ed39ea66/SK-Personnel-profile?fullscreen&hints=off

---
+++ src/sample/Supply_Portal_Controller.java
@@ -0,0 +1,46 @@
+package sample;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.animation.PauseTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import tray.animations.AnimationType;
+import tray.notification.NotificationType;
+import tray.notification.TrayNotification;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+import static java.lang.Integer.parseInt;
+
+
+
+public class Supply_Portal_Controller implements Initializable{
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) { }
+
+    @FXML
+    private void Supply_Portal_Sign_Out(ActionEvent event) {
+    }
+
+    @FXML
+    private void Supply_Portal_Close_App(ActionEvent event) {
+    }
+}
