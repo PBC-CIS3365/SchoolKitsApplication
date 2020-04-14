556d3a03126eda9177394c568e875ab0d237927c
amghl <amughalm@gmail.com>
2020-04-14 02:25:24 -0500

    Completed TableView

--- src/sample/Inventory.java
+++ src/sample/Inventory.java
@@ -1,5 +1,57 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Inventory {
-    //test1
+    private SimpleStringProperty itemName;
+    private SimpleStringProperty itemDescription;
+    private SimpleStringProperty itemBrand;
+
+    public Inventory(){
+        this.itemName = new SimpleStringProperty();
+        this.itemDescription = new SimpleStringProperty();
+        this.itemBrand = new SimpleStringProperty();
+
+    }
+    /////Name
+    public String getItemName() {
+        return itemName.get();
+    }
+
+    public SimpleStringProperty itemNameProperty() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName.set(itemName);
+    }
+
+    /////////Description
+
+    public String getItemDescription() {
+        return itemDescription.get();
+    }
+
+    public SimpleStringProperty itemDescriptionProperty() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription.set(itemDescription);
+    }
+
+    //////Brand
+
+    public String getItemBrand() {
+        return itemBrand.get();
+    }
+
+    public SimpleStringProperty itemBrandProperty() {
+        return itemBrand;
+    }
+
+    public void setItemBrand(String itemBrand) {
+        this.itemBrand.set(itemBrand);
+    }
+
 }
--- src/sample/MylistandSearch.java
+++ src/sample/MylistandSearch.java
@@ -1,8 +1,11 @@
 package sample;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -10,6 +13,8 @@ import javafx.scene.layout.Pane;
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.sql.*;
+import javafx.scene.control.*;
 
 public class MylistandSearch implements Initializable {
 
@@ -19,14 +24,37 @@ public class MylistandSearch implements Initializable {
     public AnchorPane one_pane;
     public AnchorPane two_pane;
 
+    public TableView item_table;
+    public TableColumn<Inventory, String> itemName_col;
+    public TableColumn<Inventory, String> itemDes_col;
+    public TableColumn<Inventory, String> itemBrand_col;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        itemName_col.setCellValueFactory(cellData -> cellData.getValue().itemNameProperty());
+        itemDes_col.setCellValueFactory(cellData -> cellData.getValue().itemDescriptionProperty());
+        itemBrand_col.setCellValueFactory(cellData -> cellData.getValue().itemBrandProperty());
+
+        try{
+            ObservableList<Inventory> inList = InventoryDOA.getAllRecords();
+            populateTable(inList);
+            System.out.println("ok");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+
+
 
     }
 
+    private void populateTable(ObservableList<Inventory> inList) {
+        item_table.setItems(inList);
+    }
 
 
     public void list_action(MouseEvent mouseEvent) {
